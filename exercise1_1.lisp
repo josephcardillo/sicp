d5b44efb7e2e5bf567524b935dce9a7d9432bb9b
jcardillo <jcardillo@linode.com>
2019-04-29 02:59:06 -0400

    finished exercises 1.1

--- exercise1_1.lisp
+++ exercise1_1.lisp
@@ -1,41 +1,42 @@
 ;;;; Exercise 1.1
 
 (print 10)
-; 10
+; ANSWER: 10
 
 (print
 (+ 5 3 4))
-; 12
+; ANSWER: 12
 
 (print
 (- 9 1))
-; 8
+; ANSWER: 8
 
 (print
 (/ 6 2))
-; 3
+; ANSWER: 3
 
 (print
 (+ (* 2 4) (- 4 6)))
-; 6
+; ANSWER: 6
 
 (defvar a 3)
 (print a)
-; a = 3
+; ANSWER: 3
 
 (defvar b (+ a 1))
 (print b)
-; b = 4
+; ANSWER: 4
 
 (print
 (+ a b (* a b)))
 ; a + b + the product of a and b
 ; 3 * 4 = 12
 ; 3 + 4 + 12 = 19
+; ANSWER: 19
 
 (print
 (= a b))
-; false
+; ANSWER: false / NIL
 
 (print
 (if (and (> b a) (< b (* a b)))
@@ -55,3 +56,22 @@
 ; 2 + (if b is greater than a, return b, else return a.)
 ; b is greater than a (4 > 3), so 2 + 4 = 6
 ; ANSWER: 6
+
+(print
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+    (+ a 1)))
+#|| In english: If a is great than b, then return a. Else if a is less than
+b, return b. If neither is true, return -1. Then multiply the returned value by
+the sum of a + 1.
+
+Since b = 4 and a = 3
+a is less than b.
+
+Therefore this evaluates to:
+(* 4 (+ 3 1))
+(* 4 4)
+||#
+; ANSWER: 16
+
