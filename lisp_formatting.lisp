6e9fc6069dd8b7ebb777b5d58a2596982c9604c0
jcardillo <jcardillo@linode.com>
2019-04-29 00:40:52 -0400

    formatting examples

---
+++ lisp_formatting.lisp
@@ -0,0 +1,16 @@
+;;;; Comment to describe program
+
+;;; Basic comment
+
+;; Comment indented with code
+
+; Comment after a line of code
+
+#|| 
+multi-line
+comment
+||#
+
+;;; To output data in terminal
+
+(format t "hello world ~%") ;; tilda/percent is for newline
