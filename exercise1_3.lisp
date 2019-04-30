16b8a24a16a76ce162c37aa8d2f9dbb1e9f4ff9c
jcardillo <jcardillo@linode.com>
2019-04-30 17:44:31 -0400

    exercise 1.3 in progress

--- exercise1_3.lisp
+++ exercise1_3.lisp
@@ -1,4 +1,4 @@
-;;;; Exercise 1.3
+;;;; Exercise 1.3 (in progress)
 
 #|| Define a procedure that takes three numbers as arguments and 
 returns the sum of the squares of the two larger numbers. ||#
@@ -24,4 +24,17 @@ returns the sum of the squares of the two larger numbers. ||#
 
 (square-two-larger -2 -3 4)
 ;VALUE: 20
+
+(square-two-larger 4 3 2)
+;VALUE: 25
+
+(square-two-larger 3 2 3)
+;VALUE: 18
+
+(square-two-larger 5 2 3)
+;VALUE: 34
+
+(square-two-larger 1 5 2)
+;THIS FAILS
+
 
