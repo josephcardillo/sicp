b330a5354c94e48d0a64b20c05a3cb86d851fb02
jcardillo <jcardillo@linode.com>
2019-04-30 17:35:47 -0400

    added tests

--- exercise1_3.lisp
+++ exercise1_3.lisp
@@ -1,5 +1,7 @@
 ;;;; Exercise 1.3
 
+#|| Define a procedure that takes three numbers as arguments and 
+returns the sum of the squares of the two larger numbers. ||#
 
 (define (square x) (* x x))
 
@@ -12,7 +14,8 @@
 (define (square-two-larger x y z)
 (sum-of-squares (larger-num x y) (larger-num y z)))
 
-Tests:
+;;; Tests
+
 (square-two-larger 1 2 3)
 ;VALUE: 13
 
