8727995d3fd0eda3b02ca39f632e45c29f2edbe3
jcardillo <jcardillo@linode.com>
2019-04-30 17:33:33 -0400

    added examples

--- exercise1_3.lisp
+++ exercise1_3.lisp
@@ -12,5 +12,13 @@
 (define (square-two-larger x y z)
 (sum-of-squares (larger-num x y) (larger-num y z)))
 
+Tests:
 (square-two-larger 1 2 3)
+;VALUE: 13
+
+(square-two-larger 5 5 5)
+;VALUE: 50
+
+(square-two-larger -2 -3 4)
+;VALUE: 20
 
