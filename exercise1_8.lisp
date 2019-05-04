68e4b15a828f1863e42522439649a524001106b9
jcardillo <jcardillo@linode.com>
2019-05-04 03:00:12 -0400

    exercise 1.8

--- exercise1_8.lisp
+++ exercise1_8.lisp
@@ -4,7 +4,7 @@
 is an approximation to the cube root of x, then a better approximation
 is given by the value
 
-(x/y^2 + 2) / 3
+(x/y^2 + 2y) / 3
 
 Use this formula to implement a cube-root procedure analogous to the
 square-root procedure. ||#
