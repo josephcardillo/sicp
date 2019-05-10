520d5dc609efef59c5d597aa71dc838ef47ebd9b
jcardillo <jcardillo@linode.com>
2019-05-10 01:37:51 -0400

    working on defining nth function

--- exercise1_13.rkt
+++ exercise1_13.rkt
@@ -3,6 +3,8 @@
 ;;;; Exercise 1.13 (In Progress)
 
 #||
+(define (square x) (* x x))
+
 (define (sqrt-iter guess x)
     (if (good-enough? guess x)
     guess
@@ -31,50 +33,21 @@
 (define phi1 (/ (+ 1 (sqrt 5)) 2))
 
 (define phi2 (/ (- 1 (sqrt 5)) 2))
-              
+
 (define (nth x n)
-  (nth-iter x n))
+  (nth-iter x x n))
   
-(define (nth-iter x count)
-  (cond ((= count 0) x)
-        ((> count 0)
-         (nth-iter (square x) (- count 1)))
-        (else 2)))
-
-(define (square x) (* x x))
-
-(nth 2 2)
-(nth 2 4)
-(nth 4 2)
-
-;I'm trying to create a function that takes x to the nth power.
-; need to come up with cond for neg exponents.
+(define (nth-iter x y count)
+  (cond ((= count 1) y)
+        ((> count 1)
+         (nth-iter x (* y x) (- count 1)))
+        (else 0)))
         
 
 
+(nth 2 2)
+(nth 2 3)
+
+        
 
 
-
-#||
-(define (sqrt-iter guess x)
-    (if (good-enough? guess x)
-    guess
-    (sqrt-iter (improve guess x)
-                x)))
-                
-(define (improve guess x)
-    (average guess (/ x guess)))
-
-
-(define (change guess x)
-    (abs (- guess (improve guess x))))
-
-(define (average x y)
-    (/ (+ x y) 2))
-    
-(define (sqrt x)
-    (sqrt-iter 1.0 x))
-
-(define (good-enough? guess x)
-    (< (change guess x) (* 0.001 guess)))
-||#
